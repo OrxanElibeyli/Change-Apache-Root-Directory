4b751f14ca22e4dd8870a4c3800bb48fb9c503e0
OrxanElibeyli <orxanelibeyli1@gmail.com>
2020-08-18 21:21:54 +0400

    better version

--- card.php
+++ card.php
@@ -1,7 +1,6 @@
 <?php
 
 
-
 $rootDir="";
 $str="";
 
@@ -12,8 +11,9 @@ define("CONFIG_FILE_2","/home/orxan/apache2.conf");
 
 displayHeader();
 
-if($argv[1]=="-c")
+if($argc==2 and $argv[1]=="-c")
 {
+    echo "CARD:  Current directory is setting up as Apache root directory.\n";
     $currentDir=shell_exec("pwd");
     $rootDir=$currentDir;
 
@@ -62,27 +62,74 @@ function displayHeader()
     echo "\n";
 
 
-    $bashCommand='cat '. CONFIG_FILE_1 . ' | grep "#DocumentRoot"';
+    $bashCommand='cat '. CONFIG_FILE_1 . ' | grep "DocumentRoot"';
     $str=shell_exec($bashCommand);
-    echo "\nCARD:  Your current directory is:  " . $str;
+    echo "\nCARD:  Your current directory is:  " . ltrim($str);
 }
 
 
 function getNewDirectory()
 {
     global $rootDir;
+    //$readRootDir="";
 
-    echo "\n---> Enter new root directory:\n";
+    
     //$rootDir="";
     while(true)
     {
-        $rootDir=readline($rootDir);
-        if($rootDir=="") echo "---> Please enter new root directory.\n";
-        else break;
+        echo "\nCARD:  Enter new root directory:\n--->  ";
+        $rootDir=readline();
+        if($rootDir=="") echo "CARD:  Please enter new root directory.\n--->";
+        $flag=checkDirectory($rootDir);
+        //$rootDir="";
+        if($flag==0 or $flag==1) break;
+        
+        
     }
 }
 
+function checkDirectory($dir)
+{
+    $returnValue;
+    $answer="";
 
+    if(file_exists($dir)==true) $returnValue=0;
+    else
+    {
+        echo "\nCARD:  This directory does not exists in your file system.\n";
+        
+        
+        
+        
+        while(true)
+        {
+            echo "\nCARD:  Do you want to create directory with the name of given string?   YES(y) or NO(n)\n--->  ";
+
+            $answer=readline($answer);
+            if($answer=="y")
+            {
+                echo "\nCARD:  Root permission may be wanted depend on string which you entered\n--->  ";
+                $command="sudo mkdir " . $dir;
+                if(shell_exec($command)) die("cat not create directory\n");
+                $returnValue=1;
+                break;
+            }
+            elseif($answer=="n")
+            {
+                $returnValue=2;
+                break;  
+            }
+            else
+            {
+                echo "\nCARD:  command not found.";
+            }
+        }
+        
+
+    }
+
+    return $returnValue;
+}
 function processConfigFiles()
 {
     global $rootDir;
@@ -94,7 +141,7 @@ function processConfigFiles()
 
     //echo $file;
 
-    $replace="        #DocumentRoot " . $rootDir . "\n";
+    $replace="        DocumentRoot " . $rootDir . "\n";
     $file=str_replace($str,$replace,$file);
 
     
@@ -115,15 +162,14 @@ function processConfigFiles()
         Options Indexes FollowSymLinks
         AllowOverride None
         Require all granted
-    </Directory>
-    ";
+    </Directory>\n";
 
     echo "\nCARD:  Adding datas to  your apache2.conf file: ";
     if(file_put_contents(CONFIG_FILE_2,$append,FILE_APPEND)==false) die("can not append datas to apache2.conf file");
     echo "Done\n";
 
     echo "\nCARD:  Process successfully completed:\n";
-    echo "       Apache root directory was changed from " . $str . " to " . $rootDir . ".\n";
+    echo "       Apache root directory was changed from " . ltrim($str) . " to " . $rootDir . ".\n";
     }
 
 
---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "SETUP:  setting up CARD program";
+echo "SETUP:  creating $HOME/card directory";
+
+mkdir $HOME/card;
+
+echo "SETUP:  copying card.php to $HOME/card";
+
+cp card.php $HOME/card
+
+echo "SETUP:  changing mode of card command";
+
+chmod +x card
+
+
+echo "SETUP:  adding card command to /usr/bin";
+echo "SETUP:  some commands need root privilages";
+
+sudo cp card /usr/bin
+
+echo "SETUP:  changing mode of /etc/apache2/apache2.conf file";
+sudo chmod o+w /etc/apache2/apache2.conf
+echo "SETUP:  changing mode of /etc/apache2/sites-available/000-default.conf file";
+sudo chmod o+w /etc/apache2/sites-available/000-default.conf
